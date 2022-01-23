47e099146893560c8b7c26006ce0bda83c7df27d
Dawit-Zeleke-Admassu <dawitruf@gmail.com>
2022-01-23 15:27:54 -0600

    second commit

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 console.log('This is test file')
+
+function testFunction(name, age) {
+    console.log('Name: ', name)
+    console.log('Age: ', age)
+    return
+}
+
+const sum = (num1, num2) => {
+    return num1 + num2
+}
